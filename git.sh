c38358a4a2fd2c6501d9a7dfa18071dbc7da8b1e
dslee <hopedslee@gmail.com>
2024-02-05 19:47:04 +0900

    2024-02-05

--- git.sh
+++ git.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-/usr/bin/git add /home/dslee/life
+/usr/bin/git pull origin
+/usr/bin/git add . 
 
 today=$(date +%Y-%m-%d)
 echo $today
-#/usr/bin/git commit -m "$today $1"
-/usr/bin/git commit -m "Schema change edate, etype"
+/usr/bin/git commit -m "$today"
 /usr/bin/git push -u origin master
 
 exit 0
